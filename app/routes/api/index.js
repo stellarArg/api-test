f4fb9c98ed7a6a6d8139fa5d4727438c3f6be043
Matias Dumrauf <matias.dumrauf@gmail.com>
2017-08-30 02:02:29 -0300

    implemented /api/segments

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const
-  usersRouter = require('./users-router');
+  usersRouter = require('./users-router'),
+  segmentsRouter = require('./segments-router');
 
 
 router.use('/users', usersRouter);
+router.use('/segments', segmentsRouter);
 
 
 module.exports = router;
