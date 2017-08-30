df288e98bdd5db09fdf7a0b26431ace2235ae313
Matias Dumrauf <matias.dumrauf@gmail.com>
2017-08-30 02:25:30 -0300

    added validator.test.js

---
+++ test/helpers/validator.test.js
@@ -0,0 +1,39 @@
+
+const
+  should = require('should'),
+  Validate = require('../../app/helpers/validator');
+
+const ValidationError = Validate.Error;
+
+
+describe('Helper - Validator', () => {
+
+  context('#is{FunctionName}', () => {
+
+    it('should return validated value if validation pass', () => {
+      should(Validate.isNull(null)).eql(null);
+    });
+
+    it('should throw ValidationError if validation fail', () => {
+      should(() => {
+        Validate.isNull(1);
+      }).throw(ValidationError);
+    });
+
+  });
+
+  context('#isNot{FunctionName}', () => {
+
+    it('should return validated value ifNot validation pass', () => {
+      should(Validate.isNotNull(1)).eql(1);
+    });
+
+    it('should throw ValidationError ifNot validation fail', () => {
+      should(() => {
+        Validate.isNotNull(null);
+      }).throw(ValidationError);
+    });
+
+  });
+
+});
