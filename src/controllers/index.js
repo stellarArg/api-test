938ab4f747696872b54e54f515cca02a01db6922
Gabriel Verges <stellarusiv@gmail.com>
2020-04-14 20:48:01 -0300

    feat(dynnamic): add dynamic controlers load

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,9 +3,10 @@ const upperFirst = require('lodash/upperFirst');
 const includes = require('lodash/includes');
 const reduce = require('lodash/reduce');
 
-const models = reduce(fs.readdirSync(), (modelsObj, filename) => {
+const models = reduce(fs.readdirSync(__dirname), (modelsObj, filename) => {
     if (!includes(filename, 'index.js')) {
-        modelsObj[`${upperFirst(filename)}Controller`] = include(filename);
+        // eslint-disable-next-line lodash/prefer-lodash-method
+        modelsObj[`${upperFirst(filename.replace('.js', ''))}Controller`] = include(`controllers/${filename}`);
     }
     return modelsObj;
 }, {});
