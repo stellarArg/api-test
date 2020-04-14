100991e22869559db79bc33db8958ee59c73333c
Gabriel Verges <stellarusiv@gmail.com>
2020-04-14 09:53:43 -0300

    bump(deps): upgrade deps

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,13 @@
-module.exports = {
-    CountryController: require('./country'),
-    StatusController: require('./status')
-};
+const fs = require('fs');
+const upperFirst = require('lodash/upperFirst');
+const includes = require('lodash/includes');
+const reduce = require('lodash/reduce');
+
+const models = reduce(fs.readdirSync(), (modelsObj, filename) => {
+    if (!includes(filename, 'index.js')) {
+        modelsObj[`${upperFirst(filename)}Controller`] = include(filename);
+    }
+    return modelsObj;
+}, {});
+
+module.exports = models;
